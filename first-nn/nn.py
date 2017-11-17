2911e31b6e4cf263c4300938211209b4c4b72f9e
david howard <dmh2000@gmail.com>
2017-11-17 14:47:53 -0800

    forward pass sort of working

---
+++ first-nn/nn.py
@@ -0,0 +1,252 @@
+# %matplotlib inline
+# %config InlineBackend.figure_format = 'retina'
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class NeuralNetwork(object):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        # Set number of nodes in input, hidden and output layers.
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+        # Initialize weights
+        self.weights_input_to_hidden = np.random.normal(0.0, self.input_nodes ** -0.5,
+                                                        (self.input_nodes, self.hidden_nodes))
+
+        self.weights_hidden_to_output = np.random.normal(0.0, self.hidden_nodes ** -0.5,
+                                                         (self.hidden_nodes, self.output_nodes))
+        self.lr = learning_rate
+
+        # use sigmoid activation
+        self.activation_function = lambda x: 1.0 / (1.0 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
+
+    def train(self, features, targets):
+        ''' Train the network on batch of features and targets.
+
+            Arguments
+            ---------
+
+            features: 2D array, each row is one data record, each column is a feature
+            targets: 1D array of target values
+
+        '''
+        n_records = features.shape[0]
+        delta_weights_i_h = np.zeros(self.weights_input_to_hidden.shape)
+        delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
+        for X, y in zip(features, targets):
+            final_outputs, hidden_outputs = self.forward_pass_train(X)  # Implement the forward pass function below
+            # Implement the backproagation function below
+            delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y,
+                                                                        delta_weights_i_h, delta_weights_h_o)
+        self.update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
+
+    def forward_pass_train(self, X):
+        ''' Implement forward pass here
+
+            Arguments
+            ---------
+            X: features batch
+
+        '''
+        #### Implement the forward pass here ####
+        # TODO: Hidden layer - Replace these values with your calculations.
+        X = X.reshape(1,self.weights_input_to_hidden.shape[0])
+        hidden_inputs  = self.weights_input_to_hidden * X.transpose()
+        hidden_inputs = hidden_inputs.sum(axis=0).reshape((hidden_inputs.shape[1],1))
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # TODO: Output layer - Replace these values with your calculations.
+        final_inputs = self.weights_hidden_to_output * hidden_outputs
+        final_outputs = final_inputs.sum(axis=0)
+
+        return final_outputs, hidden_outputs
+
+    def backpropagation(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o):
+        ''' Implement backpropagation
+
+            Arguments
+            ---------
+            final_outputs: output from forward pass
+            y: target (i.e. label) batch
+            delta_weights_i_h: change in weights from input to hidden layers
+            delta_weights_h_o: change in weights from hidden to output layers
+
+        '''
+        #### Implement the backward pass here ####
+        ### Backward pass ###
+
+        # TODO: Output error - Replace this value with your calculations.
+        error = None  # Output layer error is the difference between desired target and actual output.
+
+        # TODO: Calculate the hidden layer's contribution to the error
+        hidden_error = None
+
+        # TODO: Backpropagated error terms - Replace these values with your calculations.
+        output_error_term = None
+
+        hidden_error_term = None
+
+        # Weight step (input to hidden)
+        delta_weights_i_h += None
+        # Weight step (hidden to output)
+        delta_weights_h_o += None
+        return delta_weights_i_h, delta_weights_h_o
+
+    def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
+        ''' Update weights on gradient descent step
+
+            Arguments
+            ---------
+            delta_weights_i_h: change in weights from input to hidden layers
+            delta_weights_h_o: change in weights from hidden to output layers
+            n_records: number of records
+
+        '''
+        self.weights_hidden_to_output += None  # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += None  # update input-to-hidden weights with gradient descent step
+
+    def run(self, features):
+        ''' Run a forward pass through the network with input features
+
+            Arguments
+            ---------
+            features: 1D array of feature values
+        '''
+
+        #### Implement the forward pass here ####
+
+        # TODO: Hidden layer - Replace these values with your calculations.
+        hidden_inputs  = self.weights_input_to_hidden * features.transpose()
+        hidden_inputs = hidden_inputs.sum(axis=0).reshape((2,1))
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # TODO: Output layer - Replace these values with your calculations.
+        final_inputs = self.weights_hidden_to_output * hidden_outputs
+        final_outputs = final_inputs.sum(axis=0)
+        return final_outputs
+
+
+#########################################################
+# Set your hyperparameters here
+##########################################################
+iterations = 100
+learning_rate = 0.1
+hidden_nodes = 2
+output_nodes = 1
+
+
+
+data_path = '../../deep-learning/first-neural-network/Bike-Sharing-Dataset/hour.csv'
+
+rides = pd.read_csv(data_path)
+print(rides.head())
+
+rides[:24*10].plot(x='dteday', y='cnt', grid=True)
+
+dummy_fields = ['season', 'weathersit', 'mnth', 'hr', 'weekday']
+for each in dummy_fields:
+    dummies = pd.get_dummies(rides[each], prefix=each, drop_first=False)
+    rides = pd.concat([rides, dummies], axis=1)
+
+fields_to_drop = ['instant', 'dteday', 'season', 'weathersit',
+                  'weekday', 'atemp', 'mnth', 'workingday', 'hr']
+data = rides.drop(fields_to_drop, axis=1)
+print(data.head())
+
+quant_features = ['casual', 'registered', 'cnt', 'temp', 'hum', 'windspeed']
+
+# Store scalings in a dictionary so we can convert back later
+scaled_features = {}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean) / std
+
+# Save data for approximately the last 21 days
+test_data = data[-21*24:]
+
+# Now remove the test data from the data set
+data = data[:-21*24]
+
+# Separate the data into features and targets
+target_fields = ['cnt', 'casual', 'registered']
+features, targets = data.drop(target_fields, axis=1), data[target_fields]
+test_features, test_targets = test_data.drop(target_fields, axis=1), test_data[target_fields]
+
+print(test_features.shape)
+
+
+# Hold out the last 60 days or so of the remaining data as a validation set
+train_features, train_targets = features[:-60*24], targets[:-60*24]
+val_features, val_targets = features[-60*24:], targets[-60*24:]
+
+#############
+# In the my_answers.py file, fill out the TODO sections as specified
+#############
+
+def MSE(y, Y):
+    return np.mean((y-Y)**2)
+
+
+import unittest
+
+inputs = np.array([[0.5, -0.2, 0.1]])
+targets = np.array([[0.4]])
+test_w_i_h = np.array([[0.1, -0.2],
+                       [0.4, 0.5],
+                       [-0.3, 0.2]])
+test_w_h_o = np.array([[0.3],
+                       [-0.1]])
+
+
+class TestMethods(unittest.TestCase):
+    ##########
+    # Unit tests for data loading
+    ##########
+
+    def test_data_path(self):
+        # Test that file path to dataset has been unaltered
+        self.assertTrue(data_path.lower() == '../../deep-learning/first-neural-network/bike-sharing-dataset/hour.csv')
+
+    def test_data_loaded(self):
+        # Test that data frame loaded
+        self.assertTrue(isinstance(rides, pd.DataFrame))
+
+    ##########
+    # Unit tests for network functionality
+    ##########
+
+    def test_activation(self):
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        # Test that the activation function is a sigmoid
+        self.assertTrue(np.all(network.activation_function(0.5) == 1 / (1 + np.exp(-0.5))))
+
+    def test_train(self):
+        # Test that weights are updated correctly on training
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        network.train(inputs, targets)
+        self.assertTrue(np.allclose(network.weights_hidden_to_output,
+                                    np.array([[0.37275328],
+                                              [-0.03172939]])))
+        self.assertTrue(np.allclose(network.weights_input_to_hidden,
+                                    np.array([[0.10562014, -0.20185996],
+                                              [0.39775194, 0.50074398],
+                                              [-0.29887597, 0.19962801]])))
+
+    def test_run(self):
+        # Test correctness of run method
+        network = NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+
+        self.assertTrue(np.allclose(network.run(inputs), 0.09998924))
+
+
+suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
+unittest.TextTestRunner().run(suite)
