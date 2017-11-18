66cfa87d12dbddb7bf8c85925051c9cececdf707
david howard <dmh2000@gmail.com>
2017-11-17 22:21:16 -0800

    nn tests passing

--- first-nn/nn.py
+++ first-nn/nn.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class NeuralNetwork(object):
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         # Set number of nodes in input, hidden and output layers.
@@ -22,6 +23,9 @@ class NeuralNetwork(object):
 
         # use sigmoid activation
         self.activation_function = lambda x: 1.0 / (1.0 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
+
+    def dsigmoid(self, s):
+        return s * (1.0 - s)
 
     def train(self, features, targets):
         ''' Train the network on batch of features and targets.
@@ -52,14 +56,29 @@ class NeuralNetwork(object):
 
         '''
         #### Implement the forward pass here ####
-        # TODO: Hidden layer - Replace these values with your calculations.
-        X = X.reshape(1,self.weights_input_to_hidden.shape[0])
-        hidden_inputs  = self.weights_input_to_hidden * X.transpose()
-        hidden_inputs = hidden_inputs.sum(axis=0).reshape((hidden_inputs.shape[1],1))
+        # convert input to 2D array
+        features = X.reshape((1, self.input_nodes))
+
+        # =========================
+        #  compute the hidden layer
+        # =========================
+        # convert input to (1,n)
+        hidden_inputs = features.transpose()
+        # multiply by weights
+        hidden_inputs = self.weights_input_to_hidden * hidden_inputs
+        # sum the hidden nodes
+        hidden_inputs = hidden_inputs.sum(axis=0)
+        # reshape to (n,1)
+        hidden_inputs = hidden_inputs.reshape((self.hidden_nodes, 1))
+        # apply activation function
         hidden_outputs = self.activation_function(hidden_inputs)
 
-        # TODO: Output layer - Replace these values with your calculations.
+        # =====================
+        #  compute output layer
+        # =====================
+        # multiply by weights
         final_inputs = self.weights_hidden_to_output * hidden_outputs
+        # sum the output node
         final_outputs = final_inputs.sum(axis=0)
 
         return final_outputs, hidden_outputs
@@ -78,21 +97,30 @@ class NeuralNetwork(object):
         #### Implement the backward pass here ####
         ### Backward pass ###
 
-        # TODO: Output error - Replace this value with your calculations.
-        error = None  # Output layer error is the difference between desired target and actual output.
+        # Output error
+        error = (y - final_outputs)  # Output layer error is the difference between desired target and actual output.
 
-        # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = None
+        # hidden layer's contribution to the error
+        hidden_error = (hidden_outputs * (1.0 - hidden_outputs))
 
-        # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = None
+        # Backpropagated error terms
+        output_error_term = error * hidden_outputs
+        hidden_error_term = error * self.weights_hidden_to_output * hidden_error
 
-        hidden_error_term = None
+        # create a (3,2) array from hidden_error_term * X[i]
+        # that matches the shape of the delta_weights_i_h
+        # I couldn't figure out how to do this with numpy array operations
+        x = []
+        for i in range(len(X)):
+            x.append((hidden_error_term * X[i]).reshape(2))
+        x = np.array(x)
 
         # Weight step (input to hidden)
-        delta_weights_i_h += None
+        delta_weights_i_h += x
+
         # Weight step (hidden to output)
-        delta_weights_h_o += None
+        delta_weights_h_o += output_error_term
+
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -105,8 +133,9 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += None  # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += None  # update input-to-hidden weights with gradient descent step
+        for i in range(n_records):
+            self.weights_hidden_to_output += delta_weights_h_o * self.lr  # update hidden-to-output weights with gradient descent step
+            self.weights_input_to_hidden += delta_weights_i_h  * self.lr # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features
@@ -118,13 +147,51 @@ class NeuralNetwork(object):
 
         #### Implement the forward pass here ####
 
-        # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs  = self.weights_input_to_hidden * features.transpose()
-        hidden_inputs = hidden_inputs.sum(axis=0).reshape((2,1))
+        # reshape to 1d array
+        ft = features.reshape(features[0].shape)
+
+        # =========================
+        #  compute the hidden layer
+        #  by procedural loops
+        # =========================
+        # multiply by weights
+        hn = np.zeros(self.hidden_nodes)
+        wx = np.zeros(self.weights_input_to_hidden.shape)
+        for i in range(self.hidden_nodes):
+            for j in range(self.input_nodes):
+                f = ft[j]
+                w = self.weights_input_to_hidden[j, i]
+                wx[j, i] = f * w
+        # sum the inputs to the hidden nodes
+        for i in range(self.hidden_nodes):
+            for j in range(self.input_nodes):
+                hn[i] += wx[j, i]
+        # apply activation function
+        for i in range(self.hidden_nodes):
+            hn[i] = self.activation_function(hn[i])
+        # reshape to (n,1)
+        hn = hn.reshape((self.hidden_nodes, 1))
+
+        # =========================
+        # compute the hidden layer
+        # =========================
+        # reshape input to (1,n)
+        hidden_inputs = features.transpose()
+        # multiply by weights
+        hidden_inputs = self.weights_input_to_hidden * hidden_inputs
+        # sum the hidden nodes
+        hidden_inputs = hidden_inputs.sum(axis=0)
+        # reshape to (n,1)
+        hidden_inputs = hidden_inputs.reshape((self.hidden_nodes, 1))
+        # apply the activation function
         hidden_outputs = self.activation_function(hidden_inputs)
 
-        # TODO: Output layer - Replace these values with your calculations.
+        # =========================
+        # compute the output layer
+        # =========================
+        # multiply by weights
         final_inputs = self.weights_hidden_to_output * hidden_outputs
+        # sum the output
         final_outputs = final_inputs.sum(axis=0)
         return final_outputs
 
@@ -137,14 +204,12 @@ learning_rate = 0.1
 hidden_nodes = 2
 output_nodes = 1
 
-
-
 data_path = '../../deep-learning/first-neural-network/Bike-Sharing-Dataset/hour.csv'
 
 rides = pd.read_csv(data_path)
 print(rides.head())
 
-rides[:24*10].plot(x='dteday', y='cnt', grid=True)
+rides[:24 * 10].plot(x='dteday', y='cnt', grid=True)
 
 dummy_fields = ['season', 'weathersit', 'mnth', 'hr', 'weekday']
 for each in dummy_fields:
@@ -166,10 +231,10 @@ for each in quant_features:
     data.loc[:, each] = (data[each] - mean) / std
 
 # Save data for approximately the last 21 days
-test_data = data[-21*24:]
+test_data = data[-21 * 24:]
 
 # Now remove the test data from the data set
-data = data[:-21*24]
+data = data[:-21 * 24]
 
 # Separate the data into features and targets
 target_fields = ['cnt', 'casual', 'registered']
@@ -178,17 +243,17 @@ test_features, test_targets = test_data.drop(target_fields, axis=1), test_data[t
 
 print(test_features.shape)
 
-
 # Hold out the last 60 days or so of the remaining data as a validation set
-train_features, train_targets = features[:-60*24], targets[:-60*24]
-val_features, val_targets = features[-60*24:], targets[-60*24:]
+train_features, train_targets = features[:-60 * 24], targets[:-60 * 24]
+val_features, val_targets = features[-60 * 24:], targets[-60 * 24:]
+
 
 #############
 # In the my_answers.py file, fill out the TODO sections as specified
 #############
 
 def MSE(y, Y):
-    return np.mean((y-Y)**2)
+    return np.mean((y - Y) ** 2)
 
 
 import unittest
